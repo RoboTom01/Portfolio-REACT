d436654066a2381f6d296b7cb103ea01463be9cf
Thomas <tmrtw8@gmail.com>
2023-05-16 20:46:39 -0600

    skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './components/portfolio';
+
+
+function App() {
+    return (
+      <div className="profile-app">
+        <Portfolio />
+      </div>
+    );
+  }
+  
+
+
+export default App;
