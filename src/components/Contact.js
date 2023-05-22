b17382a3179102e00dd323e698d757e69bf386f1
Thomas <tmrtw8@gmail.com>
2023-05-22 00:10:12 -0600

    removedLocationMap

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,8 @@ function Contact() {
         <section id="contact" className="relative">
         <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <iframe
+            {/* location stuff */}
+            {/* <iframe
               width="100%"
               height="100%"
               title="map"
@@ -15,7 +16,7 @@ function Contact() {
               marginWidth={0}
               style={{ filter: "opacity(0.7)" }}
               src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-            />
+            /> */}
             <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
               <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
