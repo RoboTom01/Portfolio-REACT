ce54b807cf6272563d000e2f797306c4d8ae2afb
Thomas <tmrtw8@gmail.com>
2023-05-22 22:15:30 -0600

    projectsRoughDraft

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,24 +6,24 @@ function Projects() {
     return (
       <div>
         <ul id="projects">
+        <li id="Match" class="project_small">
+              <a class="project_text link" href="">Match Point</a>
+          </li>
           <li id="Hogar" class="project_small">
               <a class="project_text link" href="https://hogar-bazaar.herokuapp.com/category/7">Hogar Bazaar</a>
           </li>
           <li id="Rec" class="project_small">
               <a class="project_text link" href="https://newguyengithub.github.io/Recovery-Radar/">Recovery Radar</a>
           </li>
-          <li id="Cal" class="project_small">
-              <a class="project_text link" href="">Calculator</a>
+          <li id="Weather" class="project_small">
+              <a class="project_text link" href="https://robotom01.github.io/Weather-Dashboard/">Weather Dashboard</a>
           </li>
-          <li id="Past" class="project_small">
-              <a class="project_text link" href="">Pastel Puzzles</a>
+          <li id="Password" class="project_small">
+              <a class="project_text link" href="https://roboTom01.github.io/password_generator">Password Generator</a>
           </li>
-          <li id="Run" class="project_small">
-              <a class="project_text link" href="">Run Buddy</a>
+          <li id="Team" class="project_small">
+              <a class="project_text link" href="https://github.com/RoboTom01/Team-Profile-Generator">Team Profile Generator</a>
           </li> 
-          <li id="" class="project_small">
-              <a class="project_text link" href="">BLANK</a>
-          </li>
         </ul>
       </div>
     );
