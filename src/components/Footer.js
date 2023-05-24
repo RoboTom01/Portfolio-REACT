4d85445eaa4d1386b75568a4df3b30093d2f6ace
Thomas <tmrtw8@gmail.com>
2023-05-24 15:18:31 -0600

    footerEdit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,7 @@ function Footer () {
                         </button> */}
                         <li class="contact_list_item"><a class="href_special" href="https://github.com/RoboTom01"> 💻 Checkout my GitHub!</a></li>
                         <li class="contact_list_item"><a class="href_special" href="https://www.linkedin.com/in/thomas-m-waite-7b6594121"> 🤝 Find me on LinkedIn!</a></li>
-                        <li class="contact_list_item"><img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"></img><a class="href_special" href='https://react.dev/'>Built with React!</a></li>
+                        <li class="contact_list_item"><img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"></img><a class="href_special" href='https://react.dev/'> Built with React!</a></li>
                     </ul>
                 <div class="column_right"></div>
             </div>
