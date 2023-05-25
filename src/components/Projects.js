49e509964c2b0d0d8dc9774759712c6e373bc603
Thomas <tmrtw8@gmail.com>
2023-05-24 22:55:27 -0600

    cleanedUpProjectLinks

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,24 +6,36 @@ function Projects() {
     return (
       <div>
         <ul id="projects">
-        <li id="Match" className="project_small">
-              <a className="project_text link" href="https://pure-spire-26458.herokuapp.com/">Match Point (WIP)</a>
-          </li>
-          <li id="Hogar" className="project_small">
-              <a className="project_text link" href="https://hogar-bazaar.herokuapp.com/category/7">Hogar Bazaar</a>
-          </li>
-          <li id="Rec" className="project_small">
-              <a className="project_text link" href="https://newguyengithub.github.io/Recovery-Radar/">Recovery Radar</a>
-          </li>
-          <li id="Weather" className="project_small">
-              <a className="project_text link" href="https://robotom01.github.io/Weather-Dashboard/">Weather Dashboard</a>
-          </li>
-          <li id="Password" className="project_small">
-              <a className="project_text link" href="https://roboTom01.github.io/password_generator">Password Generator</a>
-          </li>
-          <li id="Team" className="project_small">
-              <a className="project_text link" href="https://github.com/RoboTom01/Team-Profile-Generator">Team Profile Generator</a>
-          </li> 
+          <a href="https://pure-spire-26458.herokuapp.com/">
+            <li id="Match" className="project_small">
+                <div className="project_text link">Match Point (WIP)</div>
+            </li>
+          </a>
+          <a href="https://hogar-bazaar.herokuapp.com/category/7">
+            <li id="Hogar" className="project_small">
+                <div className="project_text link">Hogar Bazaar</div>
+            </li>
+          </a>
+          <a href="https://newguyengithub.github.io/Recovery-Radar/">
+            <li id="Rec" className="project_small">
+                <div className="project_text link">Recovery Radar</div>
+            </li>
+          </a>
+          <a href="https://robotom01.github.io/Weather-Dashboard/">
+            <li id="Weather" className="project_small">
+                <div className="project_text link">Weather Dashboard</div>
+            </li>
+          </a>
+          <a href="https://roboTom01.github.io/password_generator">
+            <li id="Password" className="project_small">
+                <div className="project_text link">Password Generator</div>
+            </li>
+          </a>
+          <a href="https://github.com/RoboTom01/Team-Profile-Generator">
+            <li id="Team" className="project_small">
+                <div className="project_text link">Team Profile Generator</div>
+            </li> 
+          </a>
         </ul>
         <Footer />
       </div>
