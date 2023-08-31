6e387ec74313ddd02a33457a46498cb3ac06cba8
Thomas <tmrtw8@gmail.com>
2023-08-30 18:10:41 -0600

    profile-pic-fix-5.1

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
-import profilepic from './profilepic.jpg';
+import profilepic from '../profilepic.jpg';
 
 function About() {
     return (
