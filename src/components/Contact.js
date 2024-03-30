dca5944671d0fc5194787f63c89fdc32e8706b6d
Thomas <tmrtw8@gmail.com>
2024-03-30 17:52:56 -0600

    contactfooterfix2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,25 +21,27 @@ function Contact() {
     <div>
       <div id="email-form" className="ui form">
         <h2>Send me an email!</h2>
-        <form ref={form} onSubmit={sendEmail}>
-        <label htmlFor="name">Name:</label>
-        <div className="required field">
-          <input type="text" id="name" name='name' required />
+        <div>
+          <form ref={form} onSubmit={sendEmail}>
+            <label htmlFor="name">Name:</label>
+            <div className="required field">
+              <input type="text" id="name" name='name' required />
+            </div>
+            <label htmlFor="email">Email:</label>
+            <div className="required field">
+              <input type="email" id="email" name='email' required />
+            </div>
+            <label htmlFor="subject">Subject:</label>
+            <div className="required field">
+              <input type="text" id="subject" name='subject' required />
+            </div>
+            <label htmlFor="message">Message:</label>
+            <div className="required field">
+              <textarea id="message" name='message' required />
+            </div>
+            <button type="submit" value="Send">Send</button>
+          </form>
         </div>
-        <label htmlFor="email">Email:</label>
-        <div className="required field">
-          <input type="email" id="email" name='email' required />
-        </div>
-        <label htmlFor="subject">Subject:</label>
-        <div className="required field">
-          <input type="text" id="subject" name='subject' required />
-        </div>
-        <label htmlFor="message">Message:</label>
-        <div className="required field">
-          <textarea id="message" name='message' required />
-        </div>
-        <button type="submit" value="Send">Send</button>
-      </form>
       </div>
       <Footer />
     </div>
